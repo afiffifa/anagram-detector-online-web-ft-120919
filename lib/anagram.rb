f34b0bbc088c9d1248f518df838d8f39b4d035c3
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-22 21:07:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,4 +10,5 @@ class Anagram
     puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
     find_anagram_in_this_string.find_all do |word|
       if word.split("").sort == self.word_to_analyze.split("").sort
+        word
 
