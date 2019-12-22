262f1b4fdab19da802d664ae5e9270a5ba1ce4ef
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-22 21:07:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,5 +9,5 @@ class Anagram
   def match(find_anagram_in_this_string)
     puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
     find_anagram_in_this_string.find_all do |word|
-      if word.split("").sort == self.word_to_analyze.split
+      if word.split("").sort == self.word_to_analyze.split("")
 
